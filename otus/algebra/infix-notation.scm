20302d534e89e2e2ec80337ab69196e40b61fc17
Yuriy Chumak <yuriy.chumak@mail.com>
2023-02-14 03:56:16 +0200

    power (^) function

--- otus/algebra/infix-notation.scm
+++ otus/algebra/infix-notation.scm
@@ -23,7 +23,7 @@
    (define-macro infix-notation (lambda args
       (define priority {
          '+ 1 '- 1
-         '* 2 '/ 2
+         '* 2 '/ 2 '^ 3
          '• 2 '✕ 2
       })
       (define (operator? op)
--- otus/algebra/scalar.scm
+++ otus/algebra/scalar.scm
@@ -8,6 +8,7 @@
 
    √ ; Square Root
    ∞ ; Positive Infinity
+   ^ power ; expt
 
    ; todo: ∛ ∜ ½ ⅓ ... ⅚ ...₀₁₂₃₄₅₆₇₈₉₊₋₌₍₎ ... ¹²³⁴⁵... ０１...
    ; https://www.compart.com/en/unicode/decomposition/%3Csub%3E
@@ -18,4 +19,7 @@
 (begin
    (define √ sqrt)
    (define ∞ +inf.0)
+
+   (define ^ expt)
+   (define power expt)
 ))
